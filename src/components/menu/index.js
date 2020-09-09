7f4fd80aab85273e3abc2080343c34f4172a8f56
Vitor Fernandes <vitor.fernandes@lendico.com.br>
2020-09-09 17:06:50 -0300

    logo redirect to home

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import history from '../../assets/utils/history';
 
 import './index.css';
 import LogoMenu from '../../assets/img/logo-tc.png'
@@ -7,7 +8,7 @@ import LogoMenu from '../../assets/img/logo-tc.png'
 const SideMenu = ( ) => {
     return (
         <div className='sideMenu'>
-            <img src={LogoMenu} alt="logo" className="logo"/>
+            <img src={LogoMenu} alt="logo" className="logo" onClick={() => history.push('/')} />
         </div>
     );
 }
